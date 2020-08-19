d98291b61654a99e35499bd1adf2aa36df5f2912
Casper da Costa-Luis <tqdm@caspersci.uk.to>
2020-08-19 21:16:42 +0100

    bump version, add README

---
+++ README.rst
@@ -0,0 +1,7 @@
+Alias for typos of `tqdm <https://github.com/tqdm/tqdm>`_.
+
+It's tqdm, *taqaddum*, *te quiero demasiado*!
+
+If you don't like it... have a meta comic: `xkcd#302 <https://xkcd.com/302/>`_.
+
+Also see `tqdm#995 <https://github.com/tqdm/tqdm/issues/995>`_.
--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from io import open as io_open
+import os
 from setuptools import setup, find_packages
+
+src_dir = os.path.abspath(os.path.dirname(__file__))
+README_rst = ''
+fndoc = os.path.join(src_dir, 'README.rst')
+with io_open(fndoc, mode='r', encoding='utf-8') as fd:
+    README_rst = fd.read()
 setup(
     name='tdqm',
-    version='0.0.0',
+    version='0.0.1',
     description='Alias for typos of tqdm',
-    long_description='Alias for typos of tqdm',
+    long_description=README_rst,
     long_description_content_type='text/x-rst',
     license='MPLv2.0',
     url='https://github.com/tqdm/tqdm',
